6868607a0ec9e4bd2643e328b4fafdf19f014d42
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-02 21:06:30 -0500

    grabbed container and made loop for customers

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 console.log('main.js.linked');
 
+// grab section element that will contain customer cards
+let container = document.querySelector('#customer-container');
+
+// loop through the array of customers
 for (let customer of customers) {
-    
+    buildCustomerHtml(customer);
 }
