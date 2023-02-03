8174378a04a67f1a9878353501cc45ae8c578372
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-03 16:31:36 -0500

    reorganized JS; finished nameElement

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 console.log('main.js linked');
 
+
+// // EXAMPLE
+// <section id="customer-container">
+
+// <div class="customer-card">
+//     <img class="thumbnail" src="" alt="">
+//     <h2 class="full-name"><h2>
+//     <p class="email"></p>
+//     <p class="street-address"></p>
+//     <p class="city-state-zip"></p>
+//     <p class="birthdate"></p>
+//     <p class="date-registered"></p>
+// </div> -->
+
+// </section>
+
+
 let container = document.querySelector('#customer-container');
 // grab section element that will contain customer cards
 
@@ -7,7 +24,6 @@ for (let customer of customers) {
     buildCustomerHtml(customer);
 }
 // loop through the array of customers
-// WHAT IS BUILDCUSTOMERHTML?
 // APPLIED FROM CLASS EXAMPLE
 
 // WORKING OFF OF CLASS EXAMPLE BELOW
@@ -15,21 +31,26 @@ for (let customer of customers) {
 function buildCustomerHtml(individual) {
     // console.log(customer.name);
     // if i don't comment out line above (consol.log...) i get an error
-    // WHERE DID 'INDIVIDUAL' COME FROM?
 
     let cardElement = document.createElement('div');
-    // create a div
     cardElement.classList.add('customer-card');
-    // add a class to the div
+    // create div and add a class
+    container.appendChild(cardElement);
+    // put cardElement<div> inside container<section>
 
-    // CREATE IMG HERE?
     let thumbElement = document.createElement('img');
-    // create img
     thumbElement.classList.add('thumbnail');
-    // add a class to the img
+    // create img and add a class
+    cardElement.appendChild(thumbElement);
+    // put thumbElement<img> inside cardElement<div>
+    // NEED TO ADD SRC TO IMG
 
     let nameElement = document.createElement('h2');
-    // create h2
     nameElement.classList.add('full-name');
-    // add a class to h2
+    // create h2 and add a class
+    let text = document.createTextNode(`${individual.name.title} ${individual.name.first} ${individual.name.last}`);
+    // create a textNode for the name
+    nameElement.appendChild(text);
+    // put full-name textNode inside nameElement<h2>
+    cardElement.appendChild(nameElement);
 }
