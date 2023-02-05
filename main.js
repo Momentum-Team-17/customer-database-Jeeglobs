be56503a903f755f89df8b621aaba09371c58a75
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-05 01:10:21 -0500

    cleaned up main.js; still need to fix dates

--- main.js
+++ main.js
@@ -17,92 +17,77 @@ console.log('main.js linked');
 // </section>
 
 
+// grab <section id="customer-container"><section>
 let container = document.querySelector('#customer-container');
-// grab section element that will contain customer cards
 
+// loop function buildCustomerHtml
 for (let customer of customers) {
     buildCustomerHtml(customer);
 }
-// loop through the array of customers
 
 // FUNCTION FOR CAPITALIZING NAMES
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 
+// function that is called in the loop above
 function buildCustomerHtml(individual) {
-    // function that is called in the loop above
 
+    // cardElement === <div class="customer-card">
     let cardElement = document.createElement('div');
     cardElement.classList.add('customer-card');
-    // create cardElement<div>; add class .customer-card to cardElement<div>
     container.appendChild(cardElement);
-    // nest cardElement<div> inside container<section>
 
+    // thumbElement === <img class="thumbnail" src="(individual.picture.large")>
     let thumbElement = document.createElement('img');
     thumbElement.classList.add('thumbnail');
     thumbElement.src = individual.picture.large;
-    // create thumbElement<img>; add class .thumbnail to thumbElement<img>; add src to thumbnail<img>
     cardElement.appendChild(thumbElement);
-    // nest thumbElement<img> inside cardElement<div>
 
+    // nameElement === <h2 class="full-name">(nameText)</h2>
     let nameElement = document.createElement('h2');
     nameElement.classList.add('full-name');
-    // create nameElement<h2>; add class .full-name to nameElement<h2>
     let nameText = document.createTextNode(`${capitalizeFirstLetter(individual.name.first)} ${capitalizeFirstLetter(individual.name.last)}`);
-    // create textNode for nameElement<h2>; call function capitalizeFirstLetter in textNode
     nameElement.appendChild(nameText);
-    // put textNode inside nameElement<h2>
     cardElement.appendChild(nameElement);
-    // nest nameElement<h2> inside cardElement<div>
 
+    // emailElement === <p class="email">(emailText)</p>
     let emailElement = document.createElement('p');
     emailElement.classList.add('email');
-    // create emailElement<p>; add class .email to emailElement<p>
     let emailText = document.createTextNode(individual.email);
     emailElement.appendChild(emailText);
-    // creat textNode for emailElement<p>; put textNode inside emailElement<p>
     cardElement.appendChild(emailElement);
-    // nest emailElement<p> inside cardElement<div>
 
+    // streetElement === <p class="street-address">(streetText)</p>
     let streetElement = document.createElement('p');
     streetElement.classList.add('street-address');
-    // create streetElement<p>; add class .street-element to streetElement<p>
     let streetText = document.createTextNode(`${individual.location.street.number} ${individual.location.street.name}`);
     streetElement.appendChild(streetText);
-    // create textNode for streetElement<p>; put textNode inside streetElement<p>
     cardElement.appendChild(streetElement);
-    // nest streetElement<p> inside cardElement<div>
 
+    // cityStateElement === <p class="city-state-zip">(cityStateText)</p>
     let cityStateElement = document.createElement('p');
     cityStateElement.classList.add('city-state-zip');
-    // create cityStateElement<p>; add class .city-state-zip to cityStateElement<p>
     let cityStateText = document.createTextNode(`${individual.location.city}, ${nameToAbbr(individual.location.state)} ${individual.location.postcode}`);
-    // added comma; called nametoAbbr
     cityStateElement.appendChild(cityStateText);
-    // create textNode for cityStateElement<p>; put textNode inside cityStateElement<p>
     cardElement.appendChild(cityStateElement);
-    // nest cityStateElement<p> inside cardElement<div>
 
+    // birthdateElement === <p class="birthdate">(birthdateText)</p>
+    // moment.js used for dates--NEED TO FIX!
     let birthdateElement = document.createElement('p');
     birthdateElement.classList.add('birthdate');
-    // create birthdateElement<p>; add class .birthdate to birthdateElement<p>
     let birthdateText = document.createTextNode(`DOB: ${moment(individual.dob.date)}`);
     birthdateElement.appendChild(birthdateText);
-    // create textNode for birthdateElement<p>; put textNode inside birthdateElement<p>; moment.js used to format dates
     cardElement.appendChild(birthdateElement);
-    // nest birthdateElement<p> inside cardElement<div>
 
+    // registeredElement === <p class="date-registered">(registeredText)</p>
+    // moment.js used for dates--NEED TO FIX!
     let registeredElement = document.createElement('p');
     registeredElement.classList.add('date-registered');
-    // create registeredElement<p>; add class .date-registered to registeredElement<p>
     let registeredText = document.createTextNode(`Customer since: ${moment(individual.registered.date)}`);
     registeredElement.appendChild(registeredText);
-    // create TextNode for registeredElement<p>; put textNode inside registeredElement<p>; moment.js used to format dates
     cardElement.appendChild(registeredElement);
-    // nest registeredElement<p> inside cardElement<div>
-
 
     // USE PUSH/POP/SHIFT/ETC TO FIX DATES???
 }
